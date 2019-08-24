581f17de5f20c36f147b25d43b1654d4ee55d6ed
Vitor Mesquita <vitorloredo@hotmail.com>
2019-08-24 13:14:45 -0300

    added tabs

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -6,15 +6,14 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin {
+  TabController _tabController;
 
   @override
   void initState() {
     super.initState();
 
-    // HomeService().get().then((onValue) {
-    //   onValue;
-    // });
+    _tabController = TabController(length: 2, vsync: this);
   }
 
   @override
@@ -24,7 +23,42 @@ class _HomePageState extends State<HomePage> {
         leading: Icon(Icons.format_align_justify),
         centerTitle: false,
         title: Text("Pedidos"),
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: [
+            Tab(
+              icon: _iconTab(
+                iconData: Icons.shopping_cart,
+                text: "DISPONÍVEIS"
+              )
+            ),
+            Tab(
+              icon: _iconTab(
+                iconData: Icons.check,
+                text: "ACEITOS"
+              )
+            ),
+          ],
+        )
       ),
+      body: TabBarView(
+          children: [
+            Text("This is chat Tab View"),
+            Text("This is notification Tab View"),
+          ],
+      controller: _tabController,
+      ),
+    );
+  }
+
+  Row _iconTab({IconData iconData, String text}) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Icon(iconData),
+        SizedBox(width: 6.0,),
+        Text(text)
+      ],
     );
   }
 }
