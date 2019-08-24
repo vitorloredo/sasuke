7d7eac24aca52688d446ddd075b0b4e05750144f
Vitor Mesquita <vitorloredo@hotmail.com>
2019-08-24 13:47:46 -0300

    added colors

--- lib/interface/home.dart
+++ lib/interface/home.dart
@@ -3,4 +3,6 @@ import 'package:flutter/material.dart';
 abstract class HomeInterface {
   String get displayAndress;
   Widget get toWidget;
+  Color get color;
+  bool get read;
 }
--- lib/model/offers.dart
+++ lib/model/offers.dart
@@ -1,11 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:sasuke/interface/home.dart';
 import 'package:sasuke/model/address.dart';
 import 'package:sasuke/model/link.dart';
+import 'package:sasuke/widget/home.dart';
 
-class OffersData {
+class OffersData implements HomeInterface {
   DateTime date;
   String title;
   String user;
   AddressData address;
   Link link;
+  String state;
 
+  @override
+  String get displayAndress => address.getDisplay;
+
+  @override
+  Widget get toWidget => HomeCard(homeInterface: this);
+
+  @override
+  Color get color => Colors.blue;
+
+  @override
+  bool get read => state == 'read';
 }
