455fb1722b142e278b6f13e7cf2681b74718f011
Vitor Mesquita <vitorloredo@hotmail.com>
2019-08-24 13:48:27 -0300

    created home card

---
+++ lib/widget/home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:sasuke/interface/home.dart';
+
+class HomeCard extends StatelessWidget {
+  final HomeInterface homeInterface;
+
+  HomeCard({@required this.homeInterface});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
+}
