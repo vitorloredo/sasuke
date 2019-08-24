1db0dd5561ac409ace44e0118900031479aaa482
Vitor Mesquita <vitorloredo@hotmail.com>
2019-08-24 12:46:35 -0300

    basic service

---
+++ lib/model/home.dart
@@ -0,0 +1,6 @@
+class HomeData {
+  final String offers;
+  final String leads;
+
+  HomeData({this.leads, this.offers});
+}
--- lib/service/home.dart
+++ lib/service/home.dart
@@ -1,3 +1,14 @@
-class HomeService {
+import 'dart:convert' as convert;
+import 'package:http/http.dart' as http;
 
+class HomeService {
+  static const _url = "http://testemobile.getninjas.com.br";
+
+  Future get() async {
+    return await http.get(_url)
+      .then((response) async {
+        var jsonResponse = convert.jsonDecode(response.body);
+        return;
+      });
+  }
 }
